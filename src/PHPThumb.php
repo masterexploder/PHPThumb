74175618bb1a700cf36ea50e39ad140c975d5a8c
Mark Huot <huot@happycog.com>
2010-02-27 11:44:06 -0500

    updating to get caches deleting, commented out for now though

--- src/PHPThumb.php
+++ src/PHPThumb.php
@@ -2,8 +2,12 @@
 
 $document_path = '/'.trim($_SERVER['DOCUMENT_ROOT'], '/').($_SERVER['DOCUMENT_ROOT']!=''?'/':'');
 $document_url = 'http://'.trim($_SERVER['HTTP_HOST'], '/').'/';
+
 $cache_path = '/Users/markhuot/Sites/PHPThumb/cache/';
 $cache_uri = 'http://'.trim($_SERVER['HTTP_HOST'], '/').'/cache/';
+$cache_total_files = 3000;
+$cache_total_size = 1024*100;
+$cache_max_age = '+1 month';
 
 $params = array('src'=>false, 'w'=>false, 'h'=>false);
 $options = array('resizeUp'=>true,'jpegQuality'=>100);
@@ -14,7 +18,7 @@ $cache = md5($src.$w.$h);
 
 require_once 'ThumbLib.inc.php';
 
-if (file_exists($cache_path.$cache) && @$_SERVER['HTTP_CACHE_CONTROL'] != 'no-cache')
+if (false && file_exists($cache_path.$cache) && @$_SERVER['HTTP_CACHE_CONTROL'] != 'no-cache')
 {
 	header('Location: '.$cache_uri.$cache);
 	exit();
@@ -34,22 +38,26 @@ else
 }
 
 $thumb->show();
+flush();
 
 
 
 
 
-// function saveit($filename='', $somecontent="")
-// {
-// 	if (!$handle = fopen($filename, 'a')) {
-// 		 echo "Cannot open file ($filename)";
-// 		 exit;
-// 	}
-// 	if (fwrite($handle, date('r: ').$somecontent."\n") === FALSE) {
-// 		echo "Cannot write to file ($filename)";
-// 		exit;
-// 	}
-// 	fclose($handle);
-// }
+/*
+ * Clear Cache
+ *
+ * Check the cache files and delete extra/old ones.
+ */
+/*$caches = array();
+$dh  = opendir($cache_path);
+while (false !== ($filename = readdir($dh))) {
+	if (substr($filename, 0, 1) == '.') continue;
+	$filemtime = filemtime($cache_path.$filename);
+    $caches[$filemtime] = array(
+    	'filename' => $filename,
+    	'filemtime' => $filemtime
+    );
+}
 
-// saveit('/Users/markhuot/Desktop/memory.log', 'Memory: '.number_format(memory_get_usage()/1024, 2).' KiB');
+$delete = array_slice($caches, $cache_total_files);*/
