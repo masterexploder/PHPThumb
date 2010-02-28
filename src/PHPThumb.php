6b1b775cd4e3cb804e73d23720094f0f13553977
Mark Huot <huot@happycog.com>
2010-02-28 09:18:24 -0500

    updated config with explinations and removed debugging code

--- src/PHPThumb.php
+++ src/PHPThumb.php
@@ -1,11 +1,29 @@
 <?php
 
 // SETTINGS
+// 
+// To squeak a few more KB of memory performance out of this script replace the dynamic settings
+// here with static paths.
 
+// 
+// The path to PHPThumb
+require_once 'ThumbLib.inc.php';
+
+// 
+// The path to your web root
 $document_path = '/'.trim($_SERVER['DOCUMENT_ROOT'], '/').($_SERVER['DOCUMENT_ROOT']!=''?'/':'');
-$document_url = 'http://'.trim($_SERVER['HTTP_HOST'], '/').'/';
+
+// 
+// The path to your cache folder
 $cache_path = '/Users/markhuot/Sites/PHPThumb/cache/';
+
+// 
+// The URI to your cache folder
 $cache_uri = 'http://'.trim($_SERVER['HTTP_HOST'], '/').'/cache/';
+
+// 
+// How long caches should live. Remember, hard refreshes will also clear out your cache so you'll be
+// safe setting this pretty high
 $cache_life = '-1 month';
 
 // End configurable settings
@@ -20,18 +38,16 @@ $params = array('src'=>false, 'w'=>false, 'h'=>false);
 $options = array('resizeUp'=>true,'jpegQuality'=>100,'cache_life'=>$cache_life);
 extract(array_merge($params, $options, $_GET));
 
-$src = trim($src, '/');
 $cache = md5($src.$w.$h);
-
-require_once 'ThumbLib.inc.php';
+$cache_path.= $cache;
 
 if (
-	file_exists($cache_path.$cache) &&
-	($cache_life == false || filemtime($cache_path.$cache) > strtotime($cache_life)) &&
+	file_exists($cache_path) &&
+	($cache_life == false || filemtime($cache_path) > strtotime($cache_life)) &&
 	@$_SERVER['HTTP_CACHE_CONTROL'] != 'no-cache'
 )
 {
-	saveit('/Users/markhuot/Desktop/memory.log', 'Memory 3: '.number_format(memory_get_usage()/1024, 2).' KiB');
+	header('Content-type: image/jpeg');
 	header('Location: '.$cache_uri.$cache);
 	exit();
 }
@@ -46,57 +62,8 @@ else
 	$thumb->setOptions($options);
 	$thumb->adaptiveResize($w, $h);
 	
-	$thumb->save($cache_path.$cache);
+	$thumb->save($cache_path);
 }
-
-saveit('/Users/markhuot/Desktop/memory.log', 'Memory 1: '.number_format(memory_get_usage()/1024, 2).' KiB');
 
 $thumb->show();
 flush();
-
-saveit('/Users/markhuot/Desktop/memory.log', 'Memory 2: '.number_format(memory_get_usage()/1024, 2).' KiB');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function saveit($filename='', $somecontent="")
-{
-	if (!$handle = fopen($filename, 'a')) {
-		 echo "Cannot open file ($filename)";
-		 exit;
-	}
-	if (fwrite($handle, date('r: ').$somecontent."\n") === FALSE) {
-		echo "Cannot write to file ($filename)";
-		exit;
-	}
-	fclose($handle);
-}
